85d73ba22663c19bec47ca7c029a9264cb2fa141
Alena <83471057+alenamedved@users.noreply.github.com>
2021-10-01 15:20:31 -0500

    changed links to imgs, formated

--- js/index.js
+++ js/index.js
@@ -1,276 +1,274 @@
-document.addEventListener('DOMContentLoaded', () => {
-    //Select elements from DOM
-    const footer = document.querySelector('footer');
-    const copyright = document.createElement('p');
-    const messageForm = document.getElementsByName("leave_message")[0];
-    const messageSection = document.getElementById('messages');
-    const messageList = messageSection.querySelector('ul')
-    const skillsSection = document.getElementById('skills');
-    const skillsList = skillsSection.querySelector('ul');
-    
-    //Create variables to use
-    const myName = 'Alena Miadzvedskaya';
+document.addEventListener("DOMContentLoaded", () => {
+  //Select elements from DOM
+  const footer = document.querySelector("footer");
+  const copyright = document.createElement("p");
+  const messageForm = document.getElementsByName("leave_message")[0];
+  const messageSection = document.getElementById("messages");
+  const messageList = messageSection.querySelector("ul");
+  const skillsSection = document.getElementById("skills");
+  const skillsList = skillsSection.querySelector("ul");
+
+  //Create variables to use
+  const myName = "Alena Miadzvedskaya";
+  const today = new Date();
+  const thisYear = today.getFullYear();
+  const skills = [
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "Git",
+    "Data Analysys",
+    "Data Management",
+    "Analitical Skills",
+    "Engineering",
+    "Reserch",
+  ];
+
+  //add a label to the users message if user checked out 'friend' of 'emploeyr' radio button
+  const arrImg = [];
+  arrImg[0] = new Image();
+  arrImg[1] = new Image();
+  arrImg[0].src = "img/star_logo.png";
+  arrImg[1].src = "img/heart_logo.jpg";
+  arrImg[0].width = 16;
+  arrImg[1].width = 16;
+
+  //check if radio button is checked or not
+  function checkRadioButton() {
+    const radio = document.getElementsByName("radio");
+    let pin = document.createElement("span");
+
+    for (let i = 0; i < radio.length; i++) {
+      if (radio[i].checked) {
+        pin =
+          radio[i].id === "employer"
+            ? pin.appendChild(arrImg[i])
+            : pin.appendChild(arrImg[i]);
+      }
+    }
+    return pin;
+  }
+
+  //update messageList with messages from local storage
+  updateMessageList();
+
+  //Make message section invisible by default  if it is empty
+  if (
+    localStorage["items"] === undefined ||
+    localStorage["items"].length <= 2
+  ) {
+    messageSection.style.display = "none";
+  } else if (localStorage["items"].length > 2) {
+    messageSection.style.display = "initial";
+  }
+
+  //Function to get a current time
+  function getTime() {
     const today = new Date();
-    const thisYear = today.getFullYear();
-    const skills = [
-        'JavaScript',
-        'HTML',
-        'CSS',
-        'Git',
-        'Data Analysys',
-        'Data Management',
-        'Analitical Skills',
-        'Engineering',
-        'Reserch'
-    ];
-    
-    //add a label to the users message if user checked out 'friend' of 'emploeyr' radio button
-    const arrImg = []
-    arrImg[0] = new Image
-    arrImg[1] = new Image
-    arrImg[0].src = "img/star_logo.png"
-    arrImg[1].src = "img/heart_logo.jpg"
-    arrImg[0].width = 16
-    arrImg[1].width = 16
+    const options = {
+      weekday: "short",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    //to get rid of ',' use split and join methods
+    return today.toLocaleString(undefined, options).split(",").join(" ");
+  }
 
-    //check if radio button is checked or not
-    function checkRadioButton() {
-        const radio = document.getElementsByName('radio')
-        let pin = document.createElement('span')
+  //Function to create a button
+  function createButton(nameButton) {
+    const button = document.createElement("button");
+    button.innerText = nameButton;
+    button.type = "button";
+    button.className = nameButton.toLowerCase();
+    return button;
+  }
 
-        for (let i = 0; i < radio.length; i++) {
-            if (radio[i].checked) {
-                pin = radio[i].id === 'employer' ? pin.appendChild(arrImg[i]) : pin.appendChild(arrImg[i])
-            }
+  //Add the copyright with name and year to the footer
+  copyright.innerHTML = ` &copy; ${myName} ${thisYear}`;
+  footer.appendChild(copyright);
+
+  //Fill out the Skill section with skills from the skills array.
+  for (let i = 0; i < skills.length; i++) {
+    const skill = document.createElement("li");
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+  }
+
+  //Add  events to the Skill section using Bubbling and event Delegation
+  skillsSection.addEventListener("mouseover", (event) => {
+    if (event.target.tagName == "LI") {
+      event.target.textContent = event.target.textContent.toUpperCase();
+      event.target.style.opacity = 0.7;
+    }
+  });
+
+  skillsSection.addEventListener("mouseout", (event) => {
+    if (event.target.tagName == "LI") {
+      for (let i = 0; i < skills.length; i++) {
+        if (event.target.textContent == skills[i].toUpperCase()) {
+          event.target.textContent = skills[i];
         }
-        return pin
+      }
+      event.target.style.opacity = 1;
     }
+  });
 
-    //update messageList with messages from local storage
-    updateMessageList()
-    
-    //Make message section invisible by default  if it is empty
-    if(localStorage['items'] === undefined || localStorage['items'].length <= 2) {
-        messageSection.style.display = 'none'
-    } else if (localStorage['items'].length > 2 ) {
-        messageSection.style.display = 'initial'
-    } 
+  //Working with Message Section: filling it in with messages from the users
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
+    //Collect an input data from the user and store it in variables name, email, messageFromUser
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const messageFromUser = event.target.messageFromUser.value;
 
-    //Function to get a current time
-    function getTime() {
-        const today = new Date()
-        const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric', hour: "2-digit", minute: "2-digit" }
-        //to get rid of ',' use split and join methods
-        return today.toLocaleString(undefined, options).split(',').join(' ');
-    }
-    
-    //Function to create a button
-    function createButton(nameButton) {
-        const button = document.createElement('button')
-        button.innerText = nameButton
-        button.type = 'button'
-        button.className = nameButton.toLowerCase()
-        return button
-    }
+    //Log to the console to check if it works
+    console.log(`${name} ${email} wrote: ${messageFromUser}`);
 
+    //Document creation of Elements
+    const removeButton = createButton("Remove");
+    const editButton = createButton("Edit");
+    const doneButton = createButton("Done");
+    const newMessage = document.createElement("li");
 
-    //Add the copyright with name and year to the footer
-    copyright.innerHTML = ` &copy; ${myName} ${thisYear}`;
-    footer.appendChild(copyright);
+    //Make the Done button hidden by default
+    doneButton.style.visibility = "hidden";
 
+    newMessage.innerHTML = `<span>${messageFromUser}</span><br> <span>${getTime()}</span> from: <a href="mailto:${email}">${name}</a>&nbsp`;
 
-    //Fill out the Skill section with skills from the skills array. 
-    for (let i = 0; i < skills.length; i++) {
-        const skill = document.createElement('li');
-        skill.innerText = skills[i];
-        skillsList.appendChild(skill);
-    }
+    //Final appendants to the DOM: add created elements to the newMessage and append the newMessage to  the list.
+    newMessage.appendChild(checkRadioButton());
+    newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton);
+    newMessage.appendChild(doneButton);
 
-    //Add  events to the Skill section using Bubbling and event Delegation
-    skillsSection.addEventListener('mouseover', (event) => {
-        if (event.target.tagName == 'LI') {
-            event.target.textContent = event.target.textContent.toUpperCase();
-            event.target.style.opacity = 0.7
-        }
-    });
+    messageList.insertBefore(newMessage, messageList.firstChild);
 
-    skillsSection.addEventListener('mouseout', (event) => {
-        if (event.target.tagName == 'LI') {
-            for (let i = 0; i < skills.length; i++) {
-                if (event.target.textContent == skills[i].toUpperCase()) {
-                    event.target.textContent = skills[i] 
-                }
-            }
-            event.target.style.opacity = 1
-        }
-    });
+    //As li element was added the message section can be visible
+    messageSection.style.display = "inline-block";
 
+    //update the storage with a newMessage
+    updateStorage(newMessage);
 
-    //Working with Message Section: filling it in with messages from the users
-    messageForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+    //Resert messageForm to make it clean again and ready for new input
+    event.target.reset();
+  }); //end of messageForm eventListener
 
-        //Collect an input data from the user and store it in variables name, email, messageFromUser
-        const name = event.target.name.value
-        const email = event.target.email.value
-        const messageFromUser = event.target.messageFromUser.value
+  //add onclick event to the messageList to make the buttons interactive
+  messageList.onclick = (event) => {
+    if (event.target.tagName == "BUTTON") {
+      const button = event.target;
+      const action = button.innerText.toLowerCase();
+      const entry = button.parentNode;
 
-        //Log to the console to check if it works
-        console.log(`${name} ${email} wrote: ${messageFromUser}`)
+      //put the functions inside the Object
+      const actions = {
+        remove: () => {
+          //make the message section gone using traversal approach
+          if (entry.parentNode.children.length <= 1) {
+            console.log(entry.parentNode.children.length);
+            messageSection.style.display = "none";
+          }
 
-        //Document creation of Elements
-        const removeButton = createButton('Remove')
-        const editButton = createButton('Edit')
-        const doneButton = createButton('Done')
-        const newMessage = document.createElement('li')
+          const index = getIndex(entry);
 
-        //Make the Done button hidden by default
-        doneButton.style.visibility = 'hidden'
+          storage = getDataFromLSorage();
+          storage.splice(index, 1);
 
-        newMessage.innerHTML = (`<span>${messageFromUser}</span><br> <span>${getTime()}</span> from: <a href="mailto:${email}">${name}</a>&nbsp`)
+          localStorage.removeItem("items");
+          localStorage.setItem("items", JSON.stringify(storage));
 
-        //Final appendants to the DOM: add created elements to the newMessage and append the newMessage to  the list. 
-        newMessage.appendChild(checkRadioButton())
-        newMessage.appendChild(removeButton)
-        newMessage.appendChild(editButton)
-        newMessage.appendChild(doneButton)
+          entry.remove();
+        }, //end of remove function
 
-        messageList.insertBefore(newMessage, messageList.firstChild)
-        
-        //As li element was added the message section can be visible
-        messageSection.style.display = 'inline-block'
-        
-        //update the storage with a newMessage
-        updateStorage(newMessage)
-        
-        //Resert messageForm to make it clean again and ready for new input
-        event.target.reset()
+        edit: () => {
+          entry.querySelector("span").contentEditable = true;
+          entry.querySelector("span").style.background = "lightgray";
 
-    })//end of messageForm eventListener
+          //Add the option to edit the name line too
+          entry.querySelector("a").contentEditable = true;
+          entry.querySelector("a").style.background = "lightgray";
 
-  
-    //add onclick event to the messageList to make the buttons interactive
-    messageList.onclick = (event) => {
-        if (event.target.tagName == "BUTTON") {
-            const button = event.target
-            const action = button.innerText.toLowerCase()
-            const entry = button.parentNode
+          //Make the Done button appear when additing the content
+          button.nextSibling.style.visibility = "initial";
+        }, //end of edit function
 
-            //put the functions inside the Object
-            const actions = {
+        done: () => {
+          const list = Array.from(messageList.querySelectorAll("li"));
+          const index = getIndex(entry);
 
-                remove: () => {
-                    //make the message section gone using traversal approach
-                    if (entry.parentNode.children.length <= 1) {
-                        console.log(entry.parentNode.children.length)
-                        messageSection.style.display = 'none'
-                    }
-                    
-                    const index = getIndex(entry)
-                    
-                    storage = getDataFromLSorage()
-                    storage.splice(index, 1)
-                                       
-                    localStorage.removeItem('items')
-                    localStorage.setItem('items', JSON.stringify(storage))
-                   
-                    entry.remove()
-                    
-                },//end of remove function 
+          const editedMessage = list[index];
 
-                edit: () => {
-                    entry.querySelector('span').contentEditable = true;
-                    entry.querySelector('span').style.background = 'lightgray'
+          entry.querySelector("span").contentEditable = false;
+          entry.querySelector("span").style.background = "none";
 
-                    //Add the option to edit the name line too
-                    entry.querySelector('a').contentEditable = true;
-                    entry.querySelector('a').style.background = 'lightgray'
+          entry.querySelector("a").contentEditable = false;
+          entry.querySelector("a").style.background = "none";
 
-                    //Make the Done button appear when additing the content
-                    button.nextSibling.style.visibility = 'initial'
-                },//end of edit function
+          //Make the Done button disappear when finished with additing
+          button.style.visibility = "hidden";
 
-                done: () => {
-                    
-                    const list = Array.from(messageList.querySelectorAll('li'))
-                    const index = getIndex(entry)
-                                        
-                    const editedMessage = list[index]
-                                                           
-                    entry.querySelector('span').contentEditable = false;
-                    entry.querySelector('span').style.background = 'none'
+          storage = getDataFromLSorage();
+          storage.splice(index, 1, editedMessage.innerHTML);
 
-                    entry.querySelector('a').contentEditable = false;
-                    entry.querySelector('a').style.background = 'none'
-                   
-                    //Make the Done button disappear when finished with additing
-                    button.style.visibility = 'hidden'
-
-                    storage = getDataFromLSorage()
-                    storage.splice(index, 1, editedMessage.innerHTML)
-                    
-                    localStorage.removeItem('items')
-                    localStorage.setItem('items', JSON.stringify(storage))
-                    
-                  
-                }//end of done function
-
-            }
-            //call the function from the object actions
-            actions[action]()
-
-        }//end of if statement
-    }//end of onclick event for messageList
-
-})
-
+          localStorage.removeItem("items");
+          localStorage.setItem("items", JSON.stringify(storage));
+        }, //end of done function
+      };
+      //call the function from the object actions
+      actions[action]();
+    } //end of if statement
+  }; //end of onclick event for messageList
+});
 
 /* create and send a fetch request */
-const GITHUB_USERNAME = 'alenamedved'
+const GITHUB_USERNAME = "alenamedved";
 
 //Helper functions
 function handleRepoData(repositories) {
+  const projectSection = document.getElementById("projects");
+  const projectList = projectSection.querySelector("ul");
+  projectList.className = "projectUl";
 
-    const projectSection = document.getElementById('projects')
-    const projectList = projectSection.querySelector('ul')
-    projectList.className = 'projectUl'
-    
-    repositories.forEach((repo, i) => {
-        const project = document.createElement('li')
-        const aTag = document.createElement('a')
-        const descr = document.createElement('p')
+  repositories.forEach((repo, i) => {
+    const project = document.createElement("li");
+    const aTag = document.createElement("a");
+    const descr = document.createElement("p");
 
-        descr.className = 'repoDescr'
-        aTag.href = repo.html_url
-        aTag.target = '_blank'
-        aTag.innerText = repo.name.split('-').join(' ')
-        descr.innerText = `Description: ${repo.description}.`
-        project.className = `item${i}`
-        i = i + 1
+    descr.className = "repoDescr";
+    aTag.href = repo.html_url;
+    aTag.target = "_blank";
+    aTag.innerText = repo.name.split("-").join(" ");
+    descr.innerText = `Description: ${repo.description}.`;
+    project.className = `item${i}`;
+    i = i + 1;
 
-        project.appendChild(aTag)
-        project.appendChild(descr)
-        projectList.appendChild(project)
-    })
+    project.appendChild(aTag);
+    project.appendChild(descr);
+    projectList.appendChild(project);
+  });
 }
 
 function fetchData(url, functToDo) {
-    return fetch(url)
-        .then(checkStatus)
-        .then(resp => resp.json()) //return the json data
-        .catch(error => alert('Looks like there was a problem', error))
-        .then(result => functToDo(result))
-
+  return fetch(url)
+    .then(checkStatus)
+    .then((resp) => resp.json()) //return the json data
+    .catch((error) => alert("Looks like there was a problem", error))
+    .then((result) => functToDo(result));
 }
 
 function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response)
-    } else {
-        return Promise.reject(new Error(response.statusText))
-    }
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
 }
 //call fetch data
-fetchData(`https://api.github.com/users/${GITHUB_USERNAME}/repos`, handleRepoData)
-
+fetchData(
+  `https://api.github.com/users/${GITHUB_USERNAME}/repos`,
+  handleRepoData
+);
--- js/winnie.js
+++ js/winnie.js
@@ -1,11 +1,11 @@
 //Select Elements from DOM
-const footer = document.querySelector('footer');
-const winnieSkillsSection = document.getElementById('winnieskills');
-const winnieSkillsList = winnieSkillsSection.querySelector('ul');
-const ageSection = document.getElementById('about');
+const footer = document.querySelector("footer");
+const winnieSkillsSection = document.getElementById("winnieskills");
+const winnieSkillsList = winnieSkillsSection.querySelector("ul");
+const ageSection = document.getElementById("about");
 
-//Create variables to work 
-const myName = 'Winnie The Pooh';
+//Create variables to work
+const myName = "Winnie The Pooh";
 const winnieDOB = new Date(2020, 8, 3);
 const today = new Date();
 const thisYear = today.getFullYear();
@@ -13,28 +13,30 @@ const thisMonth = today.getMonth() + 1;
 const thisDay = today.getDate();
 let ageWinnie;
 const winnieSkills = [
-  'I can jump very high',
+  "I can jump very high",
   `I always know what time it's especially if it's time to eat`,
   `And I know when it's time to go outside for a walk`,
   `I'm an experimenter: I'm always ready to try a new stuff`,
   `I train my Mom very well`,
-  `Ok, I know how to lie down, sit and stay and I come when I'm called`
+  `Ok, I know how to lie down, sit and stay and I come when I'm called`,
 ];
 
 //Creat <p> Elements
-const copyright = document.createElement('p');
-const ageparagraph = document.createElement('p');
+const copyright = document.createElement("p");
+const ageparagraph = document.createElement("p");
 
 //Calculate ageWinnie
-if (winnieDOB.getMonth() > thisMonth && thisYear == '2021') {
+if (winnieDOB.getMonth() > thisMonth && thisYear == "2021") {
   ageWinnie = 11 - winnieDOB.getMonth() + thisMonth;
 } else {
-  ageWinnie = ((thisYear - winnieDOB.getFullYear()) * 12) + (thisMonth - winnieDOB.getMonth());
+  ageWinnie =
+    (thisYear - winnieDOB.getFullYear()) * 12 +
+    (thisMonth - winnieDOB.getMonth());
 }
 
 //Fill out a Skills Section with skills from the skills array
 for (let i = 0; i < winnieSkills.length; i++) {
-  let skill = document.createElement('li');
+  let skill = document.createElement("li");
   skill.innerText = winnieSkills[i];
   winnieSkillsList.appendChild(skill);
 }
@@ -44,105 +46,106 @@ ageparagraph.innerText = `I was born on Sep 3, 2020 and today I'm ${ageWinnie} m
 ageSection.appendChild(ageparagraph);
 
 //Fill out the copyright element and append it to the DOM
-copyright.innerHTML = `&copy; ${myName} ${thisYear} `
+copyright.innerHTML = `&copy; ${myName} ${thisYear} `;
 footer.appendChild(copyright);
 
 //number of photos to show
-const numOfPhotos = 5
+const numOfPhotos = 5;
 
 //select elements from the DOM
-let slidesConteiner = document.getElementsByClassName('slideshow-container')[0]
-let dots = document.getElementsByClassName('dots')[0]
-dots.style.textAlign = 'center'
+let slidesConteiner = document.getElementsByClassName("slideshow-container")[0];
+let dots = document.getElementsByClassName("dots")[0];
+dots.style.textAlign = "center";
 
 //fill the array with photos
-const winnieImg = []
+const winnieImg = [];
 for (i = 1; i <= numOfPhotos; i++) {
-  winnieImg[i] = new Image
-  winnieImg[i].src = `img/winniePhoto/${i}.JPG`
-
-};
+  winnieImg[i] = new Image();
+  winnieImg[i].src = `./img/winniePhoto/${i}.JPG`;
+}
 
 //fill out the DOM
-const slidesDiv = createDivSlide()
-const dotsDiv = createDots()
-slidesConteiner.appendChild(addPrevArrow())
-slidesConteiner.appendChild(addNextArrow())
+const slidesDiv = createDivSlide();
+const dotsDiv = createDots();
+slidesConteiner.appendChild(addPrevArrow());
+slidesConteiner.appendChild(addNextArrow());
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function createDivSlide() {
-
   for (let i = 1; i <= numOfPhotos; i++) {
-    let slides = document.createElement('div')
-    let slideNum = document.createElement('div')
-    let img = document.createElement('img')
+    let slides = document.createElement("div");
+    let slideNum = document.createElement("div");
+    let img = document.createElement("img");
 
-    slides.className = 'mySlides fade'
-    slideNum.className = 'numbertext'
-    slideNum.innerHTML = `${i}/${numOfPhotos}`
+    slides.className = "mySlides fade";
+    slideNum.className = "numbertext";
+    slideNum.innerHTML = `${i}/${numOfPhotos}`;
 
-    img = winnieImg[i]
-    img.style.width = '100%'
+    img = winnieImg[i];
+    img.style.width = "100%";
 
-    slides.appendChild(slideNum)
-    slides.appendChild(img)
+    slides.appendChild(slideNum);
+    slides.appendChild(img);
 
-    slidesConteiner.appendChild(slides)
+    slidesConteiner.appendChild(slides);
   }
-  return slidesConteiner
+  return slidesConteiner;
 }
 
 function createDots() {
-
   for (let i = 1; i <= numOfPhotos; i++) {
-    let dot = document.createElement('span')
-    dot.className = 'dot'
+    let dot = document.createElement("span");
+    dot.className = "dot";
     dot.addEventListener("click", () => {
-      currentSlide(i)
-    })
-    dots.appendChild(dot)
+      currentSlide(i);
+    });
+    dots.appendChild(dot);
   }
-  return dots
+  return dots;
 }
 
 function addNextArrow() {
-  const arrow = document.createElement('a')
-  arrow.className = 'next'
-  arrow.innerHTML = '&#10095;'
-  arrow.addEventListener('click', () => {
-    plusSlides(1)
-  })
-  return arrow
+  const arrow = document.createElement("a");
+  arrow.className = "next";
+  arrow.innerHTML = "&#10095;";
+  arrow.addEventListener("click", () => {
+    plusSlides(1);
+  });
+  return arrow;
 }
 
 function addPrevArrow() {
-  const arrow = document.createElement('a')
-  arrow.className = 'prev'
-  arrow.innerHTML = '&#10094;'
-  arrow.addEventListener('click', () => {
-    plusSlides(-1)
-  })
-  return arrow
+  const arrow = document.createElement("a");
+  arrow.className = "prev";
+  arrow.innerHTML = "&#10094;";
+  arrow.addEventListener("click", () => {
+    plusSlides(-1);
+  });
+  return arrow;
 }
 
 function showSlides(n) {
-  let slides = document.getElementsByClassName("mySlides")
-  let dots = document.getElementsByClassName("dot")
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -151,6 +154,4 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-
 }
-
